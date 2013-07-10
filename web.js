6f7ec6414c5116b461bc36c82690d65c1c248574
Aliaksei Kuzmin <kuzAleksAleks@gmail.com>
2013-07-10 12:19:21 +0000

    web.js is now reading from the index.html

--- web.js
+++ web.js
@@ -2,8 +2,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+var infile = "index.html";
+var buf = fs.readFileSync(infile);
+
+var outStr = buf.toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(outStr);
 });
 
 var port = process.env.PORT || 5000;
