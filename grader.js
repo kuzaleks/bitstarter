42c64c79e466ab7187eb2c1875db03206a7d4878
Aliaksei Kuzmin <kuzAleksAleks@gmail.com>
2013-07-18 13:20:39 +0000

    Commit before transmission to anather EC2 instance

--- grader.js
+++ grader.js
@@ -23,10 +23,11 @@ References:
 
 var fs = require('fs');
 var program = require('commander');
+var rest = require('restler');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
-
+var URL_DEFAULT = "http://fierce-reaches-1073.herokuapp.com"
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
@@ -39,6 +40,14 @@ var assertFileExists = function(infile) {
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
+
+var htmlCB = function(result, response) {
+    if (result instanceof Error) {
+	sys.puts('Error: ' + result.message);
+    } else {
+	fs.writeFileSync("fromurl.html", result);
+  }
+}
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
@@ -65,10 +74,21 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_addr>', 'Url to check')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    var htmlFile;  
+    if (program.url) {
+	console.log('url');
+	rest.get(program.url).on('complete', htmlCB)
+	htmlFile = "fromurl.html";
+    }
+    else {
+	htmlFile = program.file;
+    }
+    var checkJson = checkHtmlFile(htmlFile, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
+    fs.writeFileSync("out.json", outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
